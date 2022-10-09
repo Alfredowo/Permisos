31fa6fe2655f72f366640435b1e95720098a586f
Alfredowo <112134780+Alfredowo@users.noreply.github.com>
2022-10-09 16:59:09 -0500

    Se crearon las entidades ahora si

---
+++ EntidadesPermisos/Herramientas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesPermisos
+{
+    public class Herramientas
+    {
+        public Herramientas(int codigo, string nombre, string descripcion, string marca, int medida)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Marca = marca;
+            this.medida = medida;
+        }
+
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public int medida { get; set; }
+    }
+}
---
+++ EntidadesPermisos/Productos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesPermisos
+{
+    public class Productos
+    {
+        public Productos(int codigo, string nombre, string descripcion, string marca)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Marca = marca;
+        }
+
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+    }
+}
