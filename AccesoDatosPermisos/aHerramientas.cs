5cedbd02af3eedbb856d0451fd08d18191f035e6
Alfredowo <112134780+Alfredowo@users.noreply.github.com>
2022-10-09 17:07:29 -0500

    Se agrego el acceso a datos

---
+++ AccesoDatosPermisos/aHerramientas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConectarBd;
+using EntidadesPermisos;
+
+namespace AccesoDatosPermisos
+{
+    class aHerramientas
+    {
+        Base b = new Base("localhost", "root", "", "permisos", 3306);
+        public void Borrar(Herramientas Entidad)
+        {
+            b.Comando(string.Format("delete from herramientas where id = {0}",
+                Entidad.Codigo));
+        }
+
+        public void Guardar(Herramientas Entidad)
+        {
+            b.Comando(string.Format("CALL insertarherramientas({0}, '{1}', '{2}', '{3}', {4});",
+                Entidad.Codigo, Entidad.Nombre, Entidad.Descripcion, Entidad.Marca, Entidad.medida));
+        }
+
+        public DataSet Mostrar()
+        {
+            return b.Obtener
+                (string.Format("SELECT * FROM viewherramientas"), "herramientas");
+        }
+    }
+}
---
+++ AccesoDatosPermisos/aProductos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConectarBd;
+using EntidadesPermisos;
+
+namespace AccesoDatosPermisos
+{
+    public class aProductos
+    {
+        Base b = new Base("localhost", "root", "", "permisos", 3306);
+        public void Borrar(Productos Entidad)
+        {
+            b.Comando(string.Format("delete from productos where id = {0}",
+                Entidad.Codigo));
+        }
+
+        public void Guardar(Productos Entidad)
+        {
+            b.Comando(string.Format("CALL insertarproductos({0}, '{1}', '{2}', '{3}');",
+                Entidad.Codigo, Entidad.Nombre, Entidad.Descripcion, Entidad.Marca));
+        }
+
+        public DataSet Mostrar()
+        {
+            return b.Obtener
+                (string.Format("SELECT * FROM viewproductos"), "productos");
+        }
+    }
+}
