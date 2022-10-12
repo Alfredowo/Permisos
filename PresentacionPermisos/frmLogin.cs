7f46efcad1f678caf7748f26011b4fbeb8b9c980
Alfredowo <112134780+Alfredowo@users.noreply.github.com>
2022-10-12 01:00:57 -0500

    se guardo nomas

--- PresentacionPermisos/frmLogin.cs
+++ PresentacionPermisos/frmLogin.cs
@@ -29,8 +29,8 @@ namespace PresentacionPermisos
             var ds = a.Login(txtNombre.Text);
             var dt = new DataTable();
             dt = ds.Tables[0];
-            //try
-            //{
+            try
+            {
                 idUsuario = int.Parse(dt.Rows[0]["id"].ToString());
                 nombre = dt.Rows[0]["nombre"].ToString();
                 contraseña = dt.Rows[0]["pass"].ToString();
@@ -42,9 +42,9 @@ namespace PresentacionPermisos
                 }
                 else
                     MessageBox.Show("Contraseña incorrecta");
-            //}
-            //catch (Exception)
-            /*{
+            }
+            catch (Exception)
+            {
                 DialogResult rs = MessageBox.Show(string.Format("El usuario que ingreso no existe, " +
                 "quiere crear una cuenta?"), "!Atención", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (rs == DialogResult.Yes)
@@ -52,7 +52,7 @@ namespace PresentacionPermisos
                     frmUsuariosRegistro frmq = new frmUsuariosRegistro();
                     frmq.ShowDialog();
                 }
-            }*/
+            }
         }
 
         private void btnRegistrarse_Click(object sender, EventArgs e)
